65f1ceebe4e5a01d1634303562ac7a4cddf225ef
Adam <259355@student.pwr.edu.pl>
2021-11-15 17:24:36 +0100

    navbar almost done

--- src/NavOptions.js
+++ src/NavOptions.js
@@ -3,16 +3,27 @@ import './Nav.scss';
 export default function NavOptions({isOpen}){
     return(
       <>
-      <ul>
-        <li>About</li>
-        <li>Contact us</li>
-        <li>Cos</li>
-        <li>Cos</li>
+      <div className="optionList">
+        <ul>
+          <li>O Nas</li>
+          <li>Osiągnięcia</li>
+          <li>Projekty</li>
+          <li>Zaufali nam</li>
+          <li>Kontakt</li>
+          <li className="icons"><a href="https://www.facebook.com/LEMWroclaw" className="fa fa-facebook"></a></li>
+          <li className="icons"><a href="https://www.instagram.com/lemwroclaw/" className="fa fa-instagram"></a></li>
+          <li className="icons"><a href="https://www.youtube.com/channel/UCKeV5Mi90jYVxipqL6RhEyg" className="fa fa-youtube-play"></a></li>
+          <li className="icons"><a href="https://www.facebook.com/LEMWroclaw" className="fab fa-tiktok"></a></li>
         </ul>
+      </div>
       <style jsx>{`
 
           .navbar{
-            grid-template-rows: ${ isOpen ? '10vh 10vh 10vh 10vh' : '10vh 0 0 0'};
+            grid-template-rows: ${ isOpen ? '13vh' : '13vh'};
+          }
+          
+          .optionList{
+            height: ${ isOpen ? '35vh' : '0'};
           }
 
           li{
