0d4b2f4510223e2fb66a08cf0fbf7a5f04a23b9c
Adam <259355@student.pwr.edu.pl>
2021-12-06 14:06:22 +0100

    aboutUs first ide

--- src/AboutUs/aboutUs.js
+++ src/AboutUs/aboutUs.js
@@ -16,13 +16,40 @@ export default function AboutUs(){
                     <h1><i className="arrow"></i></h1>
                 </div>
                 <div className="Container_3">
-                    
+                    <div className="image_1">
+
+                    </div>
+                    <div className="text_1">
+                    <h1 className="acapits">Kim Jesteśmy?</h1>
+                        <p>
+                            Jesteśmy grupą 50 studentów działających jako Koło Naukowe Pojazdów i Robotów Mobilnych. 
+                            Tworzymy projekt Light Electric Motorcycle (LEM Wrocław) pod patronatem Politechniki Wrocławskiej. 
+                            Projektujemy i budujemy od podstaw motocykle elektryczne, 
+                            jednak w tym roku postanowiliśmy stworzyć coś zupełnie nowego, czyli elektryczny pojazd terenowy typu buggy (projekt LEM SSV).
+                        </p>
+                    </div>
                 </div>
                 <div className="Container_4">
-                    
+                    <div className="image_2">
+
+                    </div>
+                    <div className="text_1">
+                    <h1 className="acapits">Co Robimy?</h1>
+                        <p>
+                            Od 2013 roku bierzemy udział w międzynarodowych zawodach SmartMoto Challenge. Są to prestiżowe, międzynarodowe zawody motocykli elektrycznych, w których rywalizują ze sobą uczelnie z całego świata. Podczas 7 edycji zdobyliśmy 9 złotych, 5 srebrnych oraz 2 brązowe medale.
+                        </p>
+                        </div>
                 </div>
                 <div className="Container_5">
-                    
+                    <div className="image_3">
+
+                    </div>
+                    <div className="text_1">
+                    <h1 className="acapits">Co w tym Roku?</h1>
+                        <p>
+                            W tym roku stworzyliśmy motocykl wyścigowy (LEM Photon), z którym pojechaliśmy na zawody MotoStudent (najbardziej prestiżowe zawody motocyklowe dla studentów na świecie, odpowiednik MotoGP). Dzięki pracy całego zespołu zdobyliśmy 1 miejsce w kategorii Best Rookie Team. 
+                        </p>
+                    </div>
                 </div>
                 <div className="Container_6">
                     
