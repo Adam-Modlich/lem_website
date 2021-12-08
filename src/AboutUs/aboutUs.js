ad5e7f6f93dd23156cafe9106b3087169ae45755
Adam <259355@student.pwr.edu.pl>
2021-12-08 16:51:07 +0100

    animations about us done

--- src/AboutUs/aboutUs.js
+++ src/AboutUs/aboutUs.js
@@ -6,58 +6,51 @@ import $ from 'jquery';
 
 
 export default function AboutUs(){
-
-    $(window).on('scroll', function() {
-        var scrollTop = $(this).scrollTop();
-      
-        $(".image_1").css({
-            opacity: function() {
-              var elementHeight = $(this).height(),
-                opacity = ((1 - (elementHeight - scrollTop) / elementHeight));
           
-              return opacity;
-            }
-          });
-      });
       $(window).on('scroll', function() {
         var scrollTop = $(this).scrollTop();
-      
-        $(".image_2").css({
+        console.log(scrollTop);
+        var heightParent1 = $(".parent1").height();
+        var heightParent2 = $(".parent2").height();
+
+
+        $(".Container_3").children().css({
             opacity: function() {
               var elementHeight = $(this).height(),
                 opacity = ((1 - (elementHeight - scrollTop) / elementHeight));
-          
               return opacity;
             }
           });
+        $(".Container_4").children().css({
+          opacity: function() {
+            var elementHeights = $(".parent2").height(),
+              opacity = ((1 - ((elementHeights+heightParent1) - scrollTop) / elementHeights));
+            return opacity;
+          }
+        });
+
+        $(".Container_5").children().css({
+          opacity: function() {
+            var elementHeights = $(".parent3").height(),
+              opacity = ((1 - ((elementHeights+heightParent1+heightParent2) - scrollTop) / elementHeights));
+        
+            return opacity;
+          }
+        });
       });
-    //   $(window).on('scroll', function() {
-    //     var scrollTop = $(this).scrollTop();
-      
-    //     $(".image_3").css({
-    //         opacity: function() {
-    //           var elementHeight = $(this).height(),
-    //             opacity = ((1 - (elementHeight - scrollTop) / elementHeight));
-          
-    //           return opacity;
-    //         }
-    //       });
-    //   });
-
-
 
     return(
         // <>
         <div className="about">
             <div className="aboutContent">
-                <div style={{backgroundColor:"black"}}>
-                    <div className="Container_1">
-
-                    </div>
+                <div className="Container_1">
+                    {/* <h1> That's Us</h1> */}
+                        <a href="#section2"><i className="arrow"></i></a>
                 </div>
-                <div className="Container_2">
+                {/* <div className="Container_2">
                     <h1><a href="#section2"><i className="arrow"></i></a></h1>
-                </div>
+                </div> */}
+                <div className="parent1">
                 <div id="section2" className="Container_3">
                     <div className="image_1">
 
@@ -72,27 +65,32 @@ export default function AboutUs(){
                         </p>
                     </div>
                 </div>
-                <div className="Container_4">
-                    <div className="image_2">
-
-                    </div>
-                    <div className="text_2">
-                    <h1 className="acapits">Co Robimy?</h1>
-                        <p>
-                            Od 2013 roku bierzemy udział w międzynarodowych zawodach SmartMoto Challenge. Są to prestiżowe, międzynarodowe zawody motocykli elektrycznych, w których rywalizują ze sobą uczelnie z całego świata. Podczas 7 edycji zdobyliśmy 9 złotych, 5 srebrnych oraz 2 brązowe medale.
-                        </p>
-                        </div>
                 </div>
-                <div className="Container_5">
-                    <div className="image_3">
+                <div className="parent2">
+                  <div className="Container_4">
+                      <div className="image_2">
 
-                    </div>
-                    <div className="text_3">
-                    <h1 className="acapits">Co w tym Roku?</h1>
-                        <p>
-                            W tym roku stworzyliśmy motocykl wyścigowy (LEM Photon), z którym pojechaliśmy na zawody MotoStudent (najbardziej prestiżowe zawody motocyklowe dla studentów na świecie, odpowiednik MotoGP). Dzięki pracy całego zespołu zdobyliśmy 1 miejsce w kategorii Best Rookie Team. 
-                        </p>
-                    </div>
+                      </div>
+                      <div className="text_2">
+                      <h1 className="acapits">Co Robimy?</h1>
+                          <p>
+                              Od 2013 roku bierzemy udział w międzynarodowych zawodach SmartMoto Challenge. Są to prestiżowe, międzynarodowe zawody motocykli elektrycznych, w których rywalizują ze sobą uczelnie z całego świata. Podczas 7 edycji zdobyliśmy 9 złotych, 5 srebrnych oraz 2 brązowe medale.
+                          </p>
+                          </div>
+                  </div>
+                </div>
+                <div className="parent3">
+                  <div className="Container_5">
+                      <div className="image_3">
+
+                      </div>
+                      <div className="text_3">
+                      <h1 className="acapits">Co w tym Roku?</h1>
+                          <p>
+                              W tym roku stworzyliśmy motocykl wyścigowy (LEM Photon), z którym pojechaliśmy na zawody MotoStudent (najbardziej prestiżowe zawody motocyklowe dla studentów na świecie, odpowiednik MotoGP). Dzięki pracy całego zespołu zdobyliśmy 1 miejsce w kategorii Best Rookie Team. 
+                          </p>
+                      </div>
+                  </div>
                 </div>
                 <div className="Container_6">
                     
