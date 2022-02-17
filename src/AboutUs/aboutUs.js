bada64b62a6567f4c72c009c2b1e762f283c0c58
Adam <259355@student.pwr.edu.pl>
2022-02-17 17:30:12 +0100

    text added

--- src/AboutUs/aboutUs.js
+++ src/AboutUs/aboutUs.js
@@ -25,7 +25,7 @@ import Footer from '../Footer';
 export default function AboutUs(){
 
   let elekTitle = "Elektronika";
-  let elekText = "Spoke as as other again ye. Hard on to roof he drew. So sell side ye in mr evil. Longer waited mr of nature seemed. Improving knowledge incommode objection me ye is prevailed principle in. Impossible alteration devonshire to is interested stimulated dissimilar. To matter esteem polite do if."
+  let elekText = "Dział Elektro zajmuje się złożeniem napędów elektrycznych, tworzonych od podstaw w naszym garażu. Projektujemy i wykonujemy baterię o określonych wymiarach, łączymy ją ze sterownikiem i silnikiem, a następnie kształtujemy charakterystykę napędu poprzez zmianę ustawień sterownika. Ponadto, zajmujemy się tworzeniem całej wiązki elektrycznej, gdzie nasze zadanie obejmuje wyprowadzenie napięcia 12V potrzebnego do działania aparatury sterowniczej, zaprogramowanie wyświetlacza oraz połączenie manetki czy przycisków sterowniczych."
   let elekName = "Piotr Wyszyński";
 
   let prezTitle = "Prezes Koła";
@@ -39,19 +39,19 @@ export default function AboutUs(){
   // let liderText2 = "góno"
 
   let zawTitle = "Zawieszenie";
-  let zawText = "góno"
+  let zawText = "Dział Zawieszenia koncentruje się na projekcie, budowie oraz regulacji układu jezdnego pojazdu. Fazę projektową zazwyczaj rozpoczyna nasz zespół. Decydujemy jakiego amortyzatora i widelców użyjemy, co poprzedzają analizy kinematyki motocykla, gdzie nadajemy mu wszystkie charakterystyczne punkty i decydujemy jaki będzie miał charakter. "
   let zawName = "Damian Sazoński";
 
   let mechTitle = "Mechanika";
-  let mechText = "góno";
+  let mechText = "Dział Mechaniki zajmuje się projektowaniem oraz wykonaniem układu przeniesienia napędu, układu chłodzenia silnika, ergonomią kierowcy. Wykonujemy zarówno prace warsztatowe, jak i te przygotowawcze do testów czy wyścigów. Naszym celem jest uzyskanie jak najlepszych osiągów na torze z każdym kolejnym projektem.";
   let mechName = "Albert Iwanicki";
 
   let konsTitle = "Konstrukcja";
-  let konsText = "góno";
+  let konsText = "Dział Konstrukcji specjalizuje się we wszelkich konstrukcjach nośnych, obudowach czy mocowaniach. Naszym zadaniem jest projektowanie elementów pod kątem jak największego bezpieczeństwa zarówno naszego kierowcy, jak i wrażliwych komponentów wewnętrznych. Oprócz projektowania oraz modelowania sami wykonujemy nasze części poprzez przygotowanie, spawanie czy gięcie.";
   let konsName = "Piotr Minta";
 
   let markTitle = "Marketing";
-  let markText = "góno";
+  let markText = "Dział Marketingu to nie tylko media społecznościowe. Nasz dział czuwa podczas zawodów i konkursów, dbając o wizerunek całego zespołu. Jesteśmy tam, gdzie wszystkie targi motocyklowe oraz wszelkiego rodzaju eventy. Dbamy o kontakt zarówno z władzami Politechniki Wrocławskiej, jak i firmami zewnętrznymi, wspierającymi naszą działalność. ";
   let markName = "Izabela Stanula";
 
 
@@ -76,24 +76,6 @@ export default function AboutUs(){
         });
         $("#bossName").fadeIn("slow"); 
       }
-
-      // const changeTextLiders = (title, title2, texty, texty2 ,sourceImg1, sourceImg2) => {
-      //   document.getElementById('beginText2').style.display = "block";
-      //   document.getElementById('depAcapit2').style.display = "block";
-      //   document.getElementById('imgPlace2').style.display = "block";
-      //   document.getElementsByClassName('depText2')[0].style.display = "block";
-
-      //   document.getElementById('beginText').textContent = texty;
-      //   document.getElementById('beginText2').textContent = texty2;
-      //   document.getElementById('depAcapit').textContent = title;
-      //   document.getElementById('depAcapit2').textContent = title2;
-      //   // document.getElementById('firstAcapit').style.display = "none";
-
-      //   document.getElementById('imgPlace').src = sourceImg1;
-      //   document.getElementById('imgPlace2').src = sourceImg2;
-      //   $('.depText').css("width","25%")
-      //   // $(".parentText_4").animate({height:"55vh"});
-      // }
 
     return(
         <>
@@ -170,25 +152,25 @@ export default function AboutUs(){
                       </div>
                       <div className='departmentsIcons'>
                         <div onClick={() => changeText(prezTitle, prezText, prezImg, prezName)} className='prezIcon Icon'>
-                          <img src={prez}></img><h2>Prezes Koła</h2>
+                          <img src={prez} alt="Zdjęcie Prezesa"></img><h2>Prezes Koła</h2>
                         </div>
                         {/* <div onClick={() => changeTextLiders(liderTitle,liderTitle2, liderText, liderText2,liderPhotonImg, liderSSVImg)} className='liderIcon'>
                           <img src={lider}></img><h2>Liderzy</h2>
                         </div> */}
                         <div onClick={() => changeText(elekTitle,elekText,elekImg, elekName)} className='elekIcon Icon'>
-                          <img src={elek}></img><h2>Elektronika</h2>
+                          <img src={elek} alt="Zdjęcie Elektronika"></img><h2>Elektronika</h2>
                         </div>
                         <div onClick={() => changeText(konsTitle, konsText,konsImg, konsName)} className='konsIcon Icon'>
-                          <img src={kons}></img><h2>Konstrukcja</h2>
+                          <img src={kons} alt="Zdjęcie Konstrukcji"></img><h2>Konstrukcja</h2>
                         </div>
                         <div onClick={() => changeText(zawTitle, zawText,zawImg, zawName)} className='zawIcon Icon'>
-                          <img src={zaw}></img><h2>Zawieszenie</h2>
+                          <img src={zaw} alt="Zdjęcie Zawieszenia"></img><h2>Zawieszenie</h2>
                         </div>
                         <div onClick={() => changeText(markTitle, markText,markImg, markName)} className='markIcon Icon'>
-                          <img src={mark}></img><h2>Marketing</h2>
+                          <img src={mark} alt="Zdjęcie Marketingu"></img><h2>Marketing</h2>
                         </div>
                         <div onClick={() => changeText(mechTitle, mechText,mechImg, mechName)} className='mechIcon Icon'>
-                          <img src={mech}></img><h2>Mechanika</h2>
+                          <img src={mech} alt="Zdjęcie Mechaniki"></img><h2>Mechanika</h2>
                         </div>
                       </div>
 
@@ -196,35 +178,35 @@ export default function AboutUs(){
                         <h1>Nasz Zespół</h1>
                         <div className='prezIcon Icon'>
                           <h2>Prezes Koła</h2>
-                          <img src={prezImg}></img>
+                          <img src={prezImg} alt="Zdjęcie Prezesa"></img>
                         </div>
                         <div className='liderIcon Icon'>
                           <h2>Lider Photon</h2>
-                          <img src={liderPhotonImg}></img>
+                          <img src={liderPhotonImg} alt="Zdjęcie Lidera Photon"></img>
                         </div>
                         <div className='liderIcon Icon'>
                           <h2>Lider SSV</h2>
-                          <img src={liderSSVImg}></img>
+                          <img src={liderSSVImg} alt="Zdjęcie Lidera SSV"></img>
                         </div>
                         <div className='elekIcon Icon'>
                           <h2>Elektronika</h2>
-                          <img src={elekImg}></img>
+                          <img src={elekImg} alt="Zdjęcie Elektroniki"></img>
                         </div>
                         <div className='konsIcon Icon'>
                           <h2>Konstrukcja</h2>
-                          <img src={konsImg}></img>
+                          <img src={konsImg} alt="Zdjęcie Konstrukcji"></img>
                         </div>
                         <div className='zawIcon Icon'>
                           <h2>Zawieszenie</h2>
-                          <img src={zawImg}></img>
+                          <img src={zawImg} alt="Zdjęcie Zawieszenia"></img>
                         </div>
                         <div className='markIcon Icon'>
                           <h2>Marketing</h2>
-                          <img src={markImg}></img>
+                          <img src={markImg} alt="Zdjęcie Marketingu"></img>
                         </div>
                         <div className='mechIcon Icon'>
                           <h2>Mechanika</h2>
-                          <img src={mechImg}></img>
+                          <img src={mechImg} alt="Zdjęcie Mechaniki"></img>
                         </div>
                       </div>
                     </div>
@@ -237,7 +219,8 @@ export default function AboutUs(){
                       <div className="text_3">
                       <h1 className="acapits">Nasz Kierowca</h1>
                           <p>
-                              W tym roku stworzyliśmy motocykl wyścigowy (LEM Photon), z którym pojechaliśmy na zawody MotoStudent (najbardziej prestiżowe zawody motocyklowe dla studentów na świecie, odpowiednik MotoGP). Dzięki pracy całego zespołu zdobyliśmy 1 miejsce w kategorii Best Rookie Team. 
+                          Szeregi projektu LEM Wrocław wspiera utalentowany i młody kierowca, Krzysztof Borys. Dzięki niemu zespół mógł podjąć się realizacji projektu LEM Photon, przy którego realizacji czerpali z jego wiedzy oraz doświadczenia.
+                          Kojarzony bardziej pod pseudonimem Mały Kris, swoją przygodę z motocyklami zaczął w wieku 5 lat. Szybko zmieniła się ona w pasję, a wkrótce w życie zawodowe Krisa. Tworzy rodzinną markę MotoKenner oraz oferuje rzetelne szkolenia motocyklowe na swoim torze.
                           </p>
                       </div>
                   </div>
